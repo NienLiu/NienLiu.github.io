eb904a7c3a52a24676021ba00c61951dbc1296df
Yeling Liu <2257343746@qq.com>
2026-05-23 14:24:58 +0800

    Revert "[WIP] Implement front-end and back-end separation for Java project"

--- backend/src/main/java/io/github/nienliu/backend/dto/auth/AuthMeResponse.java
+++
@@ -1,9 +0,0 @@
-package io.github.nienliu.backend.dto.auth;
-
-public record AuthMeResponse(
-        Long id,
-        String username,
-        String displayName,
-        String email
-) {
-}
--- backend/src/main/java/io/github/nienliu/backend/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.nienliu.backend.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
